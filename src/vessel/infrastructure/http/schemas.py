849a9bab72f4a6cfc738ff4ab5bb799f7d41ca4b
Caio Carvalho <caiocarvalho.py@gmail.com>
2026-09-06 14:37:34 -0400

    Add max_length constraint to AccountId schema

--- src/vessel/infrastructure/http/schemas.py
+++ src/vessel/infrastructure/http/schemas.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 import msgspec
 
-AccountId = Annotated[str, msgspec.Meta(min_length=1)]
+AccountId = Annotated[str, msgspec.Meta(min_length=1, max_length=64)]
 IdempotencyKey = Annotated[str, msgspec.Meta(min_length=1, max_length=128)]
 Balance = Annotated[int, msgspec.Meta(ge=0)]
 Cents = Annotated[int, msgspec.Meta(gt=0)]
